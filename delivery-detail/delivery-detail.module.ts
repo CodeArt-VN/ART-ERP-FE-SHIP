cdd56210a254fbeeabaa98c6356303052f95dd1a
hungvq <hungvq@live.com>
2023-01-14 02:29:10 +0700

    init SHIP

---
+++ delivery-detail/delivery-detail.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { ShareModule } from 'src/app/share.module';
+import { DeliveryDetailPage } from './delivery-detail.page';
+import { FileUploadModule } from 'ng2-file-upload';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { NgOptionHighlightModule } from '@ng-select/ng-option-highlight';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DeliveryDetailPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ReactiveFormsModule,
+    FileUploadModule,
+    NgSelectModule,
+    NgOptionHighlightModule,
+    ShareModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [DeliveryDetailPage]
+})
+export class DeliveryDetailPageModule { }
