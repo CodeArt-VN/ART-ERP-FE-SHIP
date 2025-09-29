516870a623139b4dfb4c4973514fd5f08693dc64
Hùng Vũ <hungvq@live.com>
2025-09-29 18:44:44 +0700

    clean code

--- delivery/delivery.module.ts
+++ delivery/delivery.module.ts
@@ -5,10 +5,9 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { DeliveryPage } from './delivery.page';
 import { ShareModule } from 'src/app/share.module';
-import { NgOptionHighlightModule } from '@ng-select/ng-option-highlight';
 
 @NgModule({
-	imports: [IonicModule, CommonModule, FormsModule, NgOptionHighlightModule, ShareModule, RouterModule.forChild([{ path: '', component: DeliveryPage }])],
+	imports: [IonicModule, CommonModule, FormsModule, ShareModule, RouterModule.forChild([{ path: '', component: DeliveryPage }])],
 	declarations: [DeliveryPage],
 })
 export class DeliveryPageModule {}
